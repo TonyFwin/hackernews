5ee8eaaae08c41161072789c7462c584d99434b9
Tony Nguyen <tonynguyen201@gmail.com>
2018-12-05 20:13:49 -0600

    Add children to search

--- src/App.js
+++ src/App.js
@@ -47,7 +47,9 @@ class App extends Component {
     const { searchTerm, list } = this.state;
     return (
       <div className="App">
-        <Search value={searchTerm} onChange={this.onSearchChange} />
+        <Search value={searchTerm} onChange={this.onSearchChange}>
+          Search
+        </Search>
         <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss} />
       </div>
     );
@@ -56,10 +58,10 @@ class App extends Component {
 
 class Search extends Component {
   render() {
-    const { value, onChange } = this.props;
+    const { value, onChange, children } = this.props;
     return (
       <form>
-        <input type="text" value={value} onChange={onChange} />
+        {children} <input type="text" value={value} onChange={onChange} />
       </form>
     );
   }
